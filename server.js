90a44a9ce016f6b43f85c605f1166650045dbfd0
HCYTangTang <134395256+HCYTangTang@users.noreply.github.com>
2024-02-12 01:11:58 +0900

    인기브랜드 순위조회
    인기브랜드 순위조회

--- server.js
+++ server.js
@@ -132,9 +132,5 @@ app.get('/brandrank', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
-});
-
 const port = 3000;
 app.listen(port, () => console.log(`서버 PORT: ${port}`));
