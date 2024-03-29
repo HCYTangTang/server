ccdc58a070f89bda7a98c7f7147334b5695ef7f9
HCYTangTang <134395256+HCYTangTang@users.noreply.github.com>
2024-03-29 19:10:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ async function launchBrowser() {
   });
 }
 
-
 const Headers1 = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
   'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
